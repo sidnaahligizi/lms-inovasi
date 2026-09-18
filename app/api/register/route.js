fdd30805d5a2543b7a19e7c390fa0a959a645159
sidnaahligizi <sidnaahligizi@gmail.com>
2026-09-18 19:22:43 +0700

    Refactor import statement for turso

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { masterTurso, getTenantClient } from '../../../lib/turso';
+import { turso } from '../../../lib/turso';
 
 export async function POST(req) {
   try {
